5802f46d97d938b9b8bc875817da5bd1ecb945f9
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2024-05-07 22:02:46 +0200

    refactor: extract engine

---
+++ packages/examples/src/twgl-cube.ts
@@ -0,0 +1,90 @@
+import * as twgl from 'twgl.js';
+import vs from './vs.glsl?raw';
+import fs from './fs.glsl?raw';
+const m4 = twgl.m4;
+
+export default function (gl: WebGLRenderingContext) {
+  const arrays = {
+    position: [
+      1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1,
+      -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1,
+      1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1,
+      1, -1, 1, -1, -1, -1, -1, -1,
+    ],
+    normal: [
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0,
+      0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0,
+      -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1,
+      0, 0, -1,
+    ],
+    texcoord: [
+      1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1,
+      0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1,
+    ],
+    indices: [
+      0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12,
+      14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
+    ],
+  };
+  const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+  const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+  const tex = twgl.createTexture(gl, {
+    min: gl.NEAREST,
+    mag: gl.NEAREST,
+    src: [
+      255, 255, 255, 255, 192, 192, 192, 255, 192, 192, 192, 255, 255, 255, 255,
+      255,
+    ],
+  });
+  const uniforms = {
+    u_lightWorldPos: [1, 8, -10],
+    u_lightColor: [1, 0.8, 0.8, 1],
+    u_ambient: [0, 0, 0, 1],
+    u_specular: [1, 1, 1, 1],
+    u_shininess: 50,
+    u_specularFactor: 1,
+    u_diffuse: tex,
+    u_viewInverse: m4.identity(),
+    u_world: m4.identity(),
+    u_worldInverseTranspose: m4.identity(),
+    u_worldViewProjection: m4.identity(),
+  };
+
+  return (time: number) => {
+    twgl.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    time *= 0.001;
+
+    const fov = (30 * Math.PI) / 180;
+    const aspect =
+      (gl.canvas as HTMLCanvasElement).clientWidth /
+      (gl.canvas as HTMLCanvasElement).clientHeight;
+    const zNear = 0.5;
+    const zFar = 10;
+    const projection = m4.perspective(fov, aspect, zNear, zFar);
+    const eye = [1, 4, -6];
+    const target = [0, 0, 0];
+    const up = [0, 1, 0];
+
+    const camera = m4.lookAt(eye, target, up);
+    const view = m4.inverse(camera);
+    const viewProjection = m4.multiply(projection, view);
+    const world = m4.rotationY(time);
+
+    Object.assign(uniforms, {
+      u_viewInverse: camera,
+      u_world: world,
+      u_worldInverseTranspose: m4.transpose(m4.inverse(world)),
+      u_worldViewProjection: m4.multiply(viewProjection, world),
+    });
+
+    gl.useProgram(programInfo.program);
+    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+    twgl.setUniforms(programInfo, uniforms);
+    gl.drawElements(gl.TRIANGLES, bufferInfo.numElements, gl.UNSIGNED_SHORT, 0);
+  };
+}
