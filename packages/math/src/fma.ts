88ad1b79552775cbc1f3f3053ed4dabf1356bb33
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2024-07-20 22:40:50 +0200

    remove non glsl 3.0 es functions

--- packages/math/src/fma.ts
+++
@@ -1,7 +0,0 @@
-import { add } from "./add";
-import { genType } from "./genType";
-import { multiply } from "./multiply";
-
-export function fma<T extends genType>(a: T, b: T, c: T): T {
-  return add(multiply(a, b), c);
-}
