bb93d0d029a350fb7e19df97d31646ce2dfff136
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2024-09-09 22:53:52 +0200

    feat: use @miniengine/glm in examples

--- packages/examples/src/models/index.ts
+++ packages/examples/src/models/index.ts
@@ -1,3 +1,18 @@
+import {
+  cross,
+  inverse,
+  lookAt,
+  mat4,
+  multiply,
+  normalize,
+  perspective,
+  radians,
+  rotate,
+  subtract,
+  transpose,
+  vec3,
+  vec4,
+} from '@miniengine/glm';
 import { loadGltf } from '@miniengine/gltf';
 import {
   Arrays,
@@ -7,12 +22,10 @@ import {
   createTexture,
   createVertexArrayInfo,
   drawBufferInfo,
-  m4,
   resizeCanvasToDisplaySize,
   setBuffersAndAttributes,
   setDefaults,
   setUniforms,
-  v3,
 } from 'twgl.js';
 import fs from './fs.glsl?raw';
 import vs from './vs.glsl?raw';
@@ -37,17 +50,17 @@ export default async function (gl: WebGL2RenderingContext) {
   });
 
   const uniforms = {
-    u_lightWorldPos: [100, 50, -333],
-    u_lightColor: [1, 1, 1, 1],
-    u_ambient: [0.1, 0.1, 0.1, 1],
-    u_specular: [0.2, 0.2, 0.2, 1],
+    u_lightWorldPos: vec3(100, 50, -333),
+    u_lightColor: vec4(1, 1, 1, 1),
+    u_ambient: vec4(0.1, 0.1, 0.1, 1),
+    u_specular: vec4(0.2, 0.2, 0.2, 1),
     u_shininess: 32,
     u_specularFactor: 1,
     u_diffuse,
-    u_viewInverse: m4.identity(),
-    u_world: m4.identity(),
-    u_worldInverseTranspose: m4.identity(),
-    u_worldViewProjection: m4.identity(),
+    u_viewInverse: mat4(1).flat(),
+    u_world: mat4(1).flat(),
+    u_worldInverseTranspose: mat4(1).flat(),
+    u_worldViewProjection: mat4(1).flat(),
   };
 
   return (time: number) => {
@@ -59,27 +72,26 @@ export default async function (gl: WebGL2RenderingContext) {
     gl.enable(gl.CULL_FACE);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    const fov = (30 * Math.PI) / 180;
+    const fov = radians(30);
     const aspect =
       (gl.canvas as HTMLCanvasElement).clientWidth /
       (gl.canvas as HTMLCanvasElement).clientHeight;
     const zNear = 50;
     const zFar = 1000;
-    const projection = m4.perspective(fov, aspect, zNear, zFar);
-    const eye = [100, 100, -200];
-    const target = [0, 40, -11];
-    const up = [0, 1, 0];
+    const projection = perspective(fov, aspect, zNear, zFar);
+    const eye = vec3(100, 100, -200);
+    const target = vec3(0, 40, -11);
+    const up = vec3(0, 1, 0);
 
-    const camera = m4.lookAt(eye, target, up);
-    const view = m4.inverse(camera);
-    const viewProjection = m4.multiply(projection, view);
-    const world = m4.rotationY(time);
+    const view = lookAt(eye, target, up);
+    const viewProjection = multiply(projection, view);
+    const world = rotate(mat4(1), time, vec3(0, 1, 0));
 
     Object.assign(uniforms, {
-      u_viewInverse: camera,
-      u_world: world,
-      u_worldInverseTranspose: m4.transpose(m4.inverse(world)),
-      u_worldViewProjection: m4.multiply(viewProjection, world),
+      u_viewInverse: inverse(view).flat(),
+      u_world: world.flat(),
+      u_worldInverseTranspose: transpose(inverse(world)).flat(),
+      u_worldViewProjection: multiply(viewProjection, world).flat(),
     });
 
     gl.useProgram(programInfo.program);
@@ -93,16 +105,16 @@ function withNormals(arrays: Arrays): Arrays {
   const positions = (arrays.POSITION as FullArraySpec).data as Float32Array; // vec3
   const normals = new Float32Array(positions.length);
 
-  for (let i = 0; i < positions.length; i += 3) {
+  for (let i = 0; i < positions.length / 3; i += 3) {
     // positions
-    const pos1: v3.Vec3 = positions.slice((i + 0) * 3, (i + 0) * 3 + 3);
-    const pos2: v3.Vec3 = positions.slice((i + 1) * 3, (i + 1) * 3 + 3);
-    const pos3: v3.Vec3 = positions.slice((i + 2) * 3, (i + 2) * 3 + 3);
+    const pos1 = vec3(...positions.slice((i + 0) * 3, (i + 0) * 3 + 3));
+    const pos2 = vec3(...positions.slice((i + 1) * 3, (i + 1) * 3 + 3));
+    const pos3 = vec3(...positions.slice((i + 2) * 3, (i + 2) * 3 + 3));
     // edges
-    const edge1: v3.Vec3 = v3.subtract(pos2, pos1);
-    const edge2: v3.Vec3 = v3.subtract(pos3, pos1);
+    const edge1 = subtract(pos2, pos1);
+    const edge2 = subtract(pos3, pos1);
     // normal
-    const normal: v3.Vec3 = v3.normalize(v3.cross(edge1, edge2));
+    const normal = normalize(cross(edge1, edge2));
     normals[(i + 0) * 3 + 0] = normal[0];
     normals[(i + 0) * 3 + 1] = normal[1];
     normals[(i + 0) * 3 + 2] = normal[2];
