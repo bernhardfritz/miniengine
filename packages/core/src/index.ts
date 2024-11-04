2b2178bf84e40bee9320b38cf6acce0ea33b3ee1
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2024-11-04 20:31:44 +0100

    feat(core): add support for react-native

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,15 @@
+function createWebGL2RenderingContext(domNode: Element): WebGL2RenderingContext {
+  const canvas = document.createElement('canvas');
+  domNode.innerHTML = '';
+  domNode.appendChild(canvas);
+  const gl = canvas.getContext('webgl2');
+  if (gl === null) {
+    throw new Error('Error creating WebGL2 context.');
+  }
+
+  return gl;
+}
+
 /**
  * Renders an app into a browser DOM node.
  * @param app An app that you want to display. An app is an async function that returns a render loop callback.
@@ -11,17 +23,28 @@
  * render(app, document.getElementById('root')!);
  * ```
  */
-export async function render(
-  app: (gl: WebGL2RenderingContext) => Promise<(deltaTime: number) => void>,
-  domNode: Element
+export async function render(app: (gl: WebGL2RenderingContext) => Promise<(deltaTime: number) => void>, domNode: Element): Promise<void>;
+/**
+ * Renders an app into an existing WebGL2RenderingContext.
+ * @param app An app that you want to display. An app is an async function that returns a render loop callback.
+ * @param gl A {@link https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext | WebGL2RenderingContext}. miniengine will display the app you pass inside this WebGL2RenderingContext.
+ * 
+ * @example
+ * ```
+ * import { render } from '@miniengine/core';
+ * import app from './app';
+ * 
+ * const canvas = document.getElementById('canvas')!;
+ * const gl = canvas.getContext('webgl2')!;
+ * render(app, gl);
+ * ```
+ */
+export async function render<T extends WebGL2RenderingContext>(app: (gl: T) => Promise<(deltaTime: number) => void>, gl: T): Promise<void>;
+export async function render<T extends WebGL2RenderingContext = WebGL2RenderingContext>(
+  app: (gl: T) => Promise<(deltaTime: number) => void>,
+  domNodeOrGl: Element | T
 ): Promise<void> {
-  const canvas = document.createElement('canvas');
-  domNode.innerHTML = '';
-  domNode.appendChild(canvas);
-  const gl = canvas.getContext('webgl2');
-  if (gl === null) {
-    throw new Error('Error creating WebGL2 context.');
-  }
+  const gl = domNodeOrGl instanceof WebGL2RenderingContext ? domNodeOrGl : createWebGL2RenderingContext(domNodeOrGl) as T;
   const callback = await app(gl);
   let previousTime = performance.now();
   const frameRequestCallback: FrameRequestCallback = (time) => {
